bff2d0ccb05fe2c3611680014386efcd31f6ef2e
Nireeksha15 <152495239+Nireeksha15@users.noreply.github.com>
2025-12-31 17:09:10 +0530

    Update admin.py

--- app1/admin.py
+++ app1/admin.py
@@ -1,20 +1,12 @@
 from django.contrib import admin
 from .models import UserProfile, Course, Cart
 
-
-# -------- USER PROFILE --------
 admin.site.register(UserProfile)
-
-
-# -------- COURSE ADMIN --------
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'language', 'is_live')
     search_fields = ('title',)
     list_filter = ('language', 'is_live')
-
-
-# -------- CART ADMIN --------
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('user', 'course')
