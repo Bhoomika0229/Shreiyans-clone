41e5d470b7dec639def3d119dc93affd4b5450f4
Nireeksha15 <152495239+Nireeksha15@users.noreply.github.com>
2025-12-31 17:08:04 +0530

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Course , UserProfile
 
-# ================= HOME =================
+
 def home(request) :
     courses = Course.objects.all()
     return render(request, 'home.html', {'courses': courses})
 
-# ================= COURSE DETAIL =================
 def course_detail(request, course_id):
     course = get_object_or_404(Course, id=course_id)
     return render(request, 'course_detail.html', {'course': course})
 
-# ================= SIGN IN =================
+
 def signin(request):
     if request.method == 'POST':
         phone = request.POST.get('phone')
@@ -26,7 +25,7 @@ def signin(request):
 
     return render(request, 'signin.html')
 
-# ================= SIGN UP =================
+
 def signup(request):
     if request.method == 'POST':
         phone = request.POST.get('phone')
@@ -40,7 +39,7 @@ def signup(request):
 
     return render(request, 'signup.html')
 
-# ================= CART =================
+
 def cart_view(request):
     cart = request.session.get('cart', [])
     courses = Course.objects.filter(id__in=cart)
@@ -60,6 +59,6 @@ def remove_from_cart(request, course_id):
     request.session['cart'] = cart
     return redirect('cart')
 
-# ================= GOOGLE LOGIN =================
+
 def google_login(request):
     return redirect('signin')
